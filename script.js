5ffb71f452294626983347984bd2d99cf238e0eb
Caspar Danielsson <caspar.danielsson@iths.se>
2024-10-22 21:27:17 +0200

    make the ball move somewhat right...

--- script.js
+++ script.js
@@ -1,39 +1,92 @@
+let border = {
+  top: 0,
+  left: 0,
+  right: document.body.clientWidth,
+  bottom: document.body.clientHeight,
+};
+
 let ball = {
   id: "ball",
-  position: [0, 0],
-  diameter: "50px",
+  position: [border.right / 2, border.bottom / 2],
+  diameter: 50,
   radius: "1000px",
-  velocity: 4,
-  currentDirection: [-1, -1],
+  velocity: 0.5,
+  direction: [1, 1],
 };
 
 let direction = {
-  left: [1, 0],
-  right: [-1, 0],
+  right: [1, 0],
+  left: [-1, 0],
   up: [0, -1],
   down: [0, 1],
 };
 
-let border = {
-  top: 0,
-  left: 0,
-  right: document.clientWidth,
-  bottom: document.clientHeight,
-};
+// adds two vectors to get new directions
+function addVector(vec1, vec2) {
+  return [vec1[0] + vec2[0], vec1[1] + vec2[1]];
+}
 
-function changePosition(item) {
-  for (const index in item.position) {
+// updates items position by multiplying velocity with direction
+function updatePosition(item) {
+  for (let index = 0; index < item.position.length; index++) {
     item.position[index] += item.velocity * item.direction[index];
   }
   return item;
 }
 
-function updateDirection() {}
-
-function animate() {}
-
-function main() {
-  requestAnimationFrame(updatePos);
+function move(item) {
+  document.getElementById(item.id).style.left = `${item.position[0]}px`;
+  document.getElementById(item.id).style.top = `${item.position[1]}px`;
+  console.log(document.getElementById(item.id).style.left);
 }
 
-window.requestAnimationFrame(updatePos);
+//
+function checkBorderColision(item) {
+  if (item.position[1] >= border.bottom - item.diameter) {
+    return "bottom";
+  } else if (item.position[1] <= border.top - item.diameter / 2) {
+    return "top";
+  } else if (item.position[0] >= border.right - item.diameter) {
+    return "right";
+  } else if (item.position[0] <= border.left - item.diameter / 2) {
+    return "left";
+  } else {
+    return false;
+  }
+}
+
+function changeDirection(item, border) {
+  let currentDirection = item.direction;
+  switch (border) {
+    case "bottom":
+      item.direction = addVector(currentDirection, [1, -1]);
+      return item;
+    case "top":
+      item.direction = addVector(currentDirection, [-1, 1]);
+      return item;
+    case "right":
+      item.direction = addVector(currentDirection, [-1, -1]);
+      return item;
+    case "left":
+      item.direction = addVector(currentDirection, [1, 1]);
+      return item;
+  }
+}
+
+let borderColision = false;
+
+// Set ball properies
+function main() {
+  ball = updatePosition(ball);
+  borderColision = checkBorderColision(ball);
+
+  if (borderColision) {
+    ball = changeDirection(ball, borderColision);
+  }
+
+  move(ball);
+  requestAnimationFrame(main);
+  console.log(ball.position);
+}
+
+window.requestAnimationFrame(main);
